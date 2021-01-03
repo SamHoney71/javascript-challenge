a4b8839e9e3d372baeb47c2fda7a9fe1ce879684
SamHoney71 <gjeter91@gmail.com>
2021-01-03 10:10:21 -0600

    cleaned up js

--- static/js/app.js
+++ static/js/app.js
@@ -3,23 +3,9 @@ var tableData = data;
 
 // console.log(tableData)
 
-// YOUR CODE HERE!
 
 // Get a reference to the table body
 var tbody = d3.select("tbody");
-
-// // Loop through `data` and console.log each ufo sighting object
-// data.forEach(function(ufoSighting) {
-//       console.log(ufoSighting);
-//       var row =tbody.append("tr");
-
-//       Object.entries(ufoSighting).forEach(function([key, value]) {
-//         console.log(key, value);
-
-//         var cell = row.append("td");
-//         cell.text(value);        
-//       });
-// });
 
 // Select the button
 var button = d3.select("#filter-btn");
@@ -65,42 +51,3 @@ function runEnter() {
     });
 
 }
-
-
-// // Loop through `data` and console.log each ufo sighting object
-// data.forEach(function(ufoSighting) {
-//     console.log(ufoSighting);
-//     var row =tbody.append("tr");
-
-//     Object.entries(ufoSighting).forEach(function([key, value]) {
-//       console.log(key, value);
-
-//       var cell = row.append("td");
-//       cell.text(value);        
-//     });
-// });
-//     // BONUS: Calculate summary statistics for the age field of the filtered data
-
-//   // First, create an array with just the age values
-//   var ages = filteredData.map(person => person.age);
-
-//   // Next, use math.js to calculate the mean, median, mode, var, and std of the ages
-//   var mean = math.mean(ages);
-//   var median = math.median(ages);
-//   var mode = math.mode(ages);
-//   var variance = math.var(ages);
-//   var standardDeviation = math.std(ages);
-
-//   // Then, select the unordered list element by class name
-//   var list = d3.select(".summary");
-
-//   // remove any children from the list to
-//   list.html("");
-
-//   // append stats to the list
-//   list.append("li").text(`Mean: ${mean}`);
-//   list.append("li").text(`Median: ${median}`);
-//   list.append("li").text(`Mode: ${mode}`);
-//   list.append("li").text(`Variance: ${variance}`);
-//   list.append("li").text(`Standard Deviation: ${standardDeviation}`);
- 
