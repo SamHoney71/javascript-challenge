4d365e1aec558b714c5801be45afffca6f601312
SamHoney71 <gjeter91@gmail.com>
2021-01-09 09:57:14 -0600

    working on state filter

--- UFO-level-2/static/js/app.js
+++ UFO-level-2/static/js/app.js
@@ -7,6 +7,19 @@ var tableData = data;
 // Get a reference to the table body
 var tbody = d3.select("tbody");
 
+// Loop through `data` and console.log each ufo sighting object & display on table
+data.forEach(function(ufoSighting) {
+  console.log(ufoSighting);
+  var row =tbody.append("tr");
+
+  Object.entries(ufoSighting).forEach(function([key, value]) {
+    // console.log(key, value);
+
+    var cell = row.append("td");
+    cell.text(value);        
+  });
+});
+
 // Select the button
 var button = d3.select("#filter-btn");
 
@@ -25,30 +38,32 @@ function runEnter() {
     
     // Select the input element and get the raw HTML node
     // var inputElement_dtg = d3.select("#datetime-input")
+    var inputElement_dtg = d3.select("#datetime-input");
     var inputElement_state = d3.select("state-input");
 
   /////  variable for each screen ())))
   
     // Get the value property of the input element
-    // var inputValue_dtg = inputElement_dtg.property("value");
+    var inputValue_dtg = inputElement_dtg.property("value");
     var inputValue_state = inputElement_state.property("value");
-  
-    // console.log(inputValue_dtg);
-    console.log(inputValue_state);
-    console.log(tableData);
-  
-    // var filteredData = tableData.filter(sighting => sighting.datetime === inputValue & sighting.city ===inputValue);
-  
-    // console.log(filteredData);
 
-    // // clears the table to display only current results
-    // var list = d3.select("tbody");
-    // list.html("");
 
-    // // Loop through `data` and console.log each ufo sighting object
-    // filteredData.forEach(function(ufoSighting){
-    //     // console.log(ufoSighting);
-    //     var row =tbody.append("tr");
+    var filteredData = tableData;
+
+    if (inputValue_dtg) {
+      filteredData = filteredData.filter(ufoSighting => ufoSighting.datetime === inputValue_dtg);
+    }
+
+    if (inputValue_dtg) {
+      filteredData = filteredData.filter(ufoSighting => ufoSighting.state === inputValue_state);
+    }
+
+  
+    
+    // Loop through `data` and console.log each ufo sighting object
+    filteredData.forEach(function(ufoSighting){
+        // console.log(ufoSighting);
+        var row =tbody.append("tr");
 
     // Object.entries(ufoSighting).forEach(function([key, value]) {
     // //   console.log(key, value);
